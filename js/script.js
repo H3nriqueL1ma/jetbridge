d74512b8a6e0e5797682e44a867f4cc7508db155
Henrique Carlos <henriquecarlos361893@gmail.com>
2024-04-09 22:16:50 -0300

    Mudanças gerais do site

---
+++ js/script.js
@@ -0,0 +1,13 @@
+"use strict";
+function scrollMenu() {
+    const header = document.querySelector("#header");
+    if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+        header.style.backgroundColor = "rgba(57, 55, 55, 0.3)";
+        header.style.color = "#fff";
+    }
+    else {
+        header.style.backgroundColor = "transparent";
+        header.style.color = "#000";
+    }
+}
+window.onscroll = () => { scrollMenu(); };
